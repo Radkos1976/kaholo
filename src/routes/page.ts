448a57422f5bb88498eec4615eba07b5ea3bdea0
Radkos76 <radkos76@gmail.com>
2021-11-26 17:42:05 +0100

    ad

--- src/routes/page.ts
+++ src/routes/page.ts
@@ -1,3 +1,12 @@
 import { Router } from "express";
 export const mainroutes = Router();
 
+mainroutes.use((req, res, next) => {
+    res.locals.title = "Express App"
+    res.locals.session  = req.session    
+    next()
+  })
+mainroutes.post('/cmd',(req,res) => {
+    const command = req.body.command;
+    
+})
